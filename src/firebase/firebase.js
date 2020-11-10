beeb737065cca3ddbef2a61a9cee29f7259e0f36
Matthew Quade <quadem09@gmail.com>
2020-11-10 07:30:15 -0500

    woops sent up db stuff

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,12 @@ import * as firebase from 'firebase';
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const config = {
-    apiKey: "AIzaSyAlJBMc-jITYnWPJJB09FONwSbOv0Sb6z8",
-    authDomain: "expensify-2ecbc.firebaseapp.com",
-    databaseURL: "https://expensify-2ecbc.firebaseio.com",
-    projectId: "expensify-2ecbc",
-    storageBucket: "expensify-2ecbc.appspot.com",
-    messagingSenderId: "553232614159",
-    appId: "1:553232614159:web:5eda1458343e3a5a93b47a",
-    measurementId: "G-RQWDR5HN37"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   };
 
     // Initialize Firebase
